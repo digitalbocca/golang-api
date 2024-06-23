17c64444bc57cca0b4550a48be77a2dd959547c5
Gabriel Bertola Bocca <digitalbocca@gmail.com>
2024-06-23 16:26:50 -0300

    Adiciona gorm

--- main.go
+++ main.go
@@ -2,19 +2,31 @@ package main
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 type Vehicle struct {
-	ID    string `json:"id"`
-	Brand string `json:"brand"`
-	Model string `json:"model"`
+	ID        uint      `json:"id"`
+	Brand     string    `json:"brand"`
+	Model     string    `json:"model"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type User struct {
+	ID        uint      `json:"id"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
 
 func GetVehicle(c *gin.Context) {
 	vehicle := Vehicle{
-		ID:    "1",
 		Brand: "Ford",
 		Model: "Fiesta",
 	}
@@ -22,7 +34,45 @@ func GetVehicle(c *gin.Context) {
 	c.JSON(http.StatusOK, vehicle)
 }
 
+func dbConn() *gorm.DB {
+	/**
+	 * @todo Use environment variables to set the connection string
+	 */
+	dsn := "host=localhost user=admin password=admin dbname=golang-api port=5432 sslmode=disable TimeZone=America/Sao_Paulo"
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	return db
+}
+
+func migrate(db *gorm.DB, entities ...interface{}) {
+	err := db.AutoMigrate(entities...)
+	if err != nil {
+		panic("failed to migrate")
+	}
+}
+
 func main() {
+	db := dbConn()
+
+	/**
+	 * @example
+	 *
+	 * models := []interface{}{}
+	 * models = append(models, &Vehicle{})
+	 * models = append(models, &User{})
+	 */
+
+	models := []interface{}{
+		&Vehicle{},
+		&User{},
+	}
+
+	migrate(db, models...)
+
 	router := gin.Default()
 
 	router.GET("/vehicle", GetVehicle)
